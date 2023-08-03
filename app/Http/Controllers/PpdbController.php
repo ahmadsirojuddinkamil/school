39d0a067341913cfd42a111ecc142b36dc712e87
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-08-03 20:52:25 +0700

    feat(siswa): add ppdb registration feature | ticket 01

--- app/Http/Controllers/PpdbController.php
+++ app/Http/Controllers/PpdbController.php
@@ -3,23 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePpdbRequest;
+use App\Models\Siswa;
 use App\Services\CreatePpdbService;
+use DateTime;
 
 class PpdbController extends Controller
 {
     public function index()
     {
-        return view('pages.ppdb.index');
+        $today = new DateTime();
+        $minDate = date_modify(clone $today, '-21 years')->format('Y-m-d');
+        return view('pages.ppdb.index', compact('today', 'minDate'));
     }
 
     public function store(StorePpdbRequest $Request, CreatePpdbService $CreatePpdbService)
     {
         $ValidateData = $Request->validated();
 
-        $CreatePpdbService->SaveDataSiswaPpdb($ValidateData);
+        $CheckIfDataAlreadyExists = Siswa::where('email', $ValidateData['email'])
+            ->orWhere('nisn', $ValidateData['nisn'])
+            ->first();
 
-        return redirect('/ppdb')->with([
-            'success' => 'Anda berhasil mendaftar ppdb',
-        ]);
+        if ($CheckIfDataAlreadyExists) {
+            return redirect('/ppdb')->with(['error' => 'NISN dan Email sudah terdaftar!']);
+        }
+
+        $CreatePpdbService->SaveDataSiswaPpdb($ValidateData);
+        return redirect('/ppdb')->with(['success' => 'Data ppdb anda berhasil dikirim! Tolong check email dalam 24 jam']);
     }
 }
--- resources/views/layouts/ppdb/index.blade.php
+++ resources/views/layouts/ppdb/index.blade.php
@@ -1,44 +1,59 @@
-<div class="container">
+<div class="container border">
+    @if (session()->has('success'))
+        <div class="alert alert-success d-flex justify-content-center" role="alert">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @if (session()->has('error'))
+        <div class="alert alert-warning d-flex justify-content-center" role="alert">
+            {{ session('error') }}
+        </div>
+    @endif
+
     <form action="/ppdb" method="POST" enctype="multipart/form-data">
         @csrf
 
-        <h4 class="d-flex justify-content-center">Data Siswa :</h4>
+        <h4 class="d-flex justify-content-center mt-3">Data Siswa :</h4>
 
         <div class="mb-3">
             <label for="email-siswa" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email-siswa" value="example@gmail.com" name="email">
+            <input type="email" class="form-control" id="email-siswa" value="example@gmail.com" name="email"
+                required>
         </div>
 
         <div class="mb-3">
             <label for="nisn-siswa" class="form-label">NISN</label>
-            <input type="number" class="form-control" id="nisn-siswa" value="0201034059" name="nisn">
+            <input type="number" class="form-control" id="nisn-siswa" value="0201034059" name="nisn" required>
         </div>
 
         <div class="mb-3">
             <label for="nama-siswa" class="form-label">Nama Lengkap</label>
-            <input type="text" class="form-control" id="nama-siswa" value="Yudi Amanda" name="nama_lengkap">
+            <input type="text" class="form-control" id="nama-siswa" value="Yudi Amanda" name="nama_lengkap" required>
         </div>
 
         <div class="mb-3">
             <label for="asal-sekolah-siswa" class="form-label">Asal Sekolah</label>
             <input type="text" class="form-control" id="asal-sekolah-siswa" value="smp negeri 1 semarang"
-                name="asal_sekolah">
+                name="asal_sekolah" required>
         </div>
 
         <div class="mb-3">
             <label for="alamat-siswa" class="form-label">Alamat</label>
-            <input type="text" class="form-control" id="alamat-siswa" value="jl. kebon jeruk" name="alamat">
+            <input type="text" class="form-control" id="alamat-siswa" value="jl. kebon jeruk" name="alamat"
+                required>
         </div>
 
         <div class="mb-3">
             <label for="telpon-siswa" class="form-label">No Telp / WhatsApp</label>
-            <input type="number" class="form-control" id="telpon-siswa" value="08121060302" name="telpon_siswa">
+            <input type="number" class="form-control" id="telpon-siswa" value="08121060302" name="telpon_siswa"
+                required maxlength="12">
         </div>
 
         <div class="mb-3">
             <label class="form-label">Jenis Kelamin</label>
 
-            <select class="form-select" name="jenis_kelamin">
+            <select class="form-select" name="jenis_kelamin" required>
                 <option value="laki-laki">Laki - laki</option>
                 <option value="perempuan">Perempuan</option>
             </select>
@@ -46,18 +61,21 @@
 
         <div class="mb-3">
             <label for="tempat-lahir-siswa" class="form-label">Tempat Lahir</label>
-            <input type="text" class="form-control" id="tempat-lahir-siswa" value="Jakarta" name="tempat_lahir">
+            <input type="text" class="form-control" id="tempat-lahir-siswa" value="Jakarta" name="tempat_lahir"
+                required>
         </div>
 
         <div class="mb-3">
             <label for="tanggal-lahir-siswa" class="form-label">Tanggal Lahir</label>
-            <input type="date" class="form-control" id="tanggal-lahir-siswa" name="tanggal_lahir">
+            <input type="date" class="form-control" id="tanggal-lahir-siswa" name="tanggal_lahir" required
+                min="<?php echo $minDate; ?>" max="<?php echo $today->format('Y-m-d'); ?>">
         </div>
+
 
         <div class="mb-4">
             <label class="form-label">Bidang Peminatan</label>
 
-            <select class="form-select" name="jurusan">
+            <select class="form-select" name="jurusan" required>
                 <option value="teknik komputer jaringan">Teknik komputer jaringan</option>
                 <option value="rekayasa perangkat lunak">Rekayasa perangkat lunak</option>
                 <option value="multimedia">Multimedia</option>
@@ -68,18 +86,18 @@
 
         <div class="mb-3">
             <label for="nama-ayah" class="form-label">Nama Ayah</label>
-            <input type="text" class="form-control" id="nama-ayah" value="Sumandi" name="nama_ayah">
+            <input type="text" class="form-control" id="nama-ayah" value="Sumandi" name="nama_ayah" required>
         </div>
 
         <div class="mb-3">
             <label for="nama-ibu" class="form-label">Nama Ibu</label>
-            <input type="text" class="form-control" id="nama-ibu" value="Siti" name="nama_ibu">
+            <input type="text" class="form-control" id="nama-ibu" value="Siti" name="nama_ibu" required>
         </div>
 
         <div class="mb-3">
             <label for="telpon-orang-tua" class="form-label">No Telp / WhatsApp</label>
             <input type="number" class="form-control" id="telpon-orang-tua" value="081232722384"
-                name="telpon_orang_tua">
+                name="telpon_orang_tua" required maxlength="12">
         </div>
 
         <div class="mb-3">
@@ -87,7 +105,7 @@
                 A.N RAHARJO</label>
             <div class="text-danger mb-3">Simpan bukti pembayaran!</div>
             <input type="file" class="form-control" id="uang-pendaftaran" accept=".jpg, .jpeg, .png"
-                name="bukti_pendaftaran_siswa_baru">
+                name="bukti_pendaftaran_siswa_baru" required>
         </div>
 
         <div id="image-preview-container" class=" mb-2">
@@ -95,7 +113,7 @@
                 style="display: none; max-width: 300px; max-height: 300px;">
         </div>
 
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
             Kirim Data
         </button>
 
@@ -107,6 +125,10 @@
                         <h5 class="modal-title" id="exampleModalLabel">Apakah data yang di isi sudah benar?</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal"
                             aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                        Setelah tekan tombol ya, silahkan check email dalam 24 jam!
                     </div>
 
                     <div class="modal-footer">
